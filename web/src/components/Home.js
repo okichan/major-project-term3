498652e73ed4bf61c8b555ad363aa8c9658f72a2
Tomomi <madeinwatashi@gmail.com>
2018-01-09 17:31:26 +1100

    Remove obsolete lines

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home({ signedIn }) {
    return (
-      <div className=" test">
+      <div >
          <h3 className="mb-5">Welcome to Tanto Sales Management System</h3>
          <h4>Today's weather: xxx</h4>
          <h4>1 AUD = 89 JPY</h4>
