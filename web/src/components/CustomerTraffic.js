6590e362b3a83a37a50f1b06e229807c8e5896d2
Your Name here <chymeng24@gmail.com>
2018-01-11 16:07:07 +1100

    Style customer traffic

--- web/src/components/CustomerTraffic.js
+++ web/src/components/CustomerTraffic.js
@@ -2,29 +2,60 @@ import React from "react"
 
 function CustomerTraffic({ traffics }) {
   return (
-    <table className="table table-hover table-sm">
-      <thread className="customerTable">
-        <tr>
-          <th scope="col">Date</th>
-          <th scope="col">Time</th>
-          <th scope="col">Count</th>
-          <th scope="col">Occupation</th>
-          <th scope="col">Weather</th>
-        </tr>
-      </thread>
-
-      <tbody>
-        {traffics.map(traffic => (
+    <div>
+      <h1 className="text-center trafficHeading">Customer Traffic</h1>
+      <table className="table table-hover table-sm text-center">
+        <thead>
           <tr>
-            <th scope="row">{traffic.date}</th>
-            <td>{traffic.time}</td>
-            <td>{traffic.count} people</td>
-            <td>{traffic.isChef}</td>
-            <td>{traffic.weather}</td>
+            <th scope="col">Date</th>
+            <th scope="col">Time</th>
+            <th scope="col">Count</th>
+            <th scope="col">Occupation</th>
+            <th scope="col">Weather</th>
+            <th scope="col">Options</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {traffics.map(traffic => (
+            <tr className="text-center">
+              <th scope="row">{traffic.date}</th>
+              <td>{traffic.time}</td>
+              <td>{traffic.count} people</td>
+              <td>{traffic.isChef}</td>
+              <td>{traffic.weather}</td>
+              <td>
+                <a href="#">
+                  <i
+                    className="fa fa-pencil-square-o med"
+                    id="edit"
+                    title="Edit"
+                  />
+                </a>
+                <span className="mr-2"> </span>
+                <i
+                  className="fa fa-trash med"
+                  id="trash"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    alert("delete function here")
+                  }}
+                  title="Delete"
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="btnContainer">
+        <button
+          type="button"
+          className="trafficBtn btn btn-default btn-lg btn-block info"
+        >
+          Add
+        </button>
+      </div>
+    </div>
   )
 }
 
