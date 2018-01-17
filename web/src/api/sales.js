cfb4bbed0d87d2264e2773b952c783036f47afad
Tomomi <madeinwatashi@gmail.com>
2018-01-17 13:33:28 +1100

    Identify Sales report bug - "Sale Trend Knife and Stone"

--- web/src/api/sales.js
+++ web/src/api/sales.js
@@ -5,6 +5,19 @@ export function listSales() {
     .then((res) => res.data)
 }
 
+export function createSale(data) {
+   return api.post('/sales', data)
+     .then((res) => res.data)
+ }
+
+ export function updateSale(id, data) {
+   return api.put(`/sale/${id}`, data)
+     .then((res) => res.data)
+ }
+ 
+ export function dailySales(date) {
+   return api.get(`/sales?date=${date}`).then(res => res.data);
+ }
 export function monthRangeSales(range) {
   return api.get(`/sales?monthRange=${range}`).then(res => res.data);
 }
