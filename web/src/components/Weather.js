6dd3437d160af307fab87c8521e4a49a39212a1a
Your Name here <chymeng24@gmail.com>
2018-01-23 14:05:37 +1100

    Add 5 days of weather

--- web/src/components/Weather.js
+++ web/src/components/Weather.js
@@ -1,5 +1,10 @@
 import React, { Component, Fragment } from "react";
 import { fetchWeather } from "../api/weather";
+import moment from "moment";
+
+fetchWeather().then(res => {
+  console.log("Loaded Movie", res);
+});
 
 class Weather extends Component {
   state = {
@@ -16,29 +21,84 @@ class Weather extends Component {
         this.setState({ error: error });
       });
   }
-
   render() {
     const { weather, error } = this.state;
     return (
-      <div className="row m-1 text-center">
-        <div className="col-md-6 mx-auto ">
+      <div className="">
+        <div className="">
           {!!error && <p>{error.message}</p>}
-
           {!!weather ? (
             <Fragment>
-              <h3> {weather.city_name} </h3>
-              <div className="row">
-                <div className="col">
+              <h3 className="text-center"> {weather.city_name} </h3>
+              <div className="row-fuid text-center">
+                <div className="col-md-6 mx-auto">
                   <img
                     src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.weather.icon
+                      weather.data[0].weather.icon
                     }.png`}
                     alt="icon"
                   />
+                  <p>{weather.data[0].datetime}</p>
+                  <h2>{`${weather.data[0].temp} \u2103`} </h2>
+                  <p>{weather.data[0].weather.description}</p>
                 </div>
-                <div className="col text-center">
-                  <h2>{`${weather.temp} \u2103`} </h2>
-                  <p>{weather.weather.description}</p>
+              </div>
+
+              <div className="d-flex flex-nowrap">
+                <div className="p-2">
+                  <img
+                    src={`https://www.weatherbit.io/static/img/icons/${
+                      weather.data[8].weather.icon
+                    }.png`}
+                    alt="icon"
+                  />
+                  <p>{weather.data[8].datetime}</p>
+                  <p>{`${weather.data[8].temp} \u2103`} </p>
+                  <p>{weather.data[8].weather.description}</p>
+                </div>
+                <div className="p-2">
+                  <img
+                    src={`https://www.weatherbit.io/static/img/icons/${
+                      weather.data[16].weather.icon
+                    }.png`}
+                    alt="icon"
+                  />
+                  <p>{weather.data[16].datetime}</p>
+                  <p>{`${weather.data[16].temp} \u2103`} </p>
+                  <p>{weather.data[16].weather.description}</p>
+                </div>
+                <div className="p-2">
+                  <img
+                    src={`https://www.weatherbit.io/static/img/icons/${
+                      weather.data[24].weather.icon
+                    }.png`}
+                    alt="icon"
+                  />
+                  <p>{weather.data[24].datetime}</p>
+                  <p>{`${weather.data[24].temp} \u2103`} </p>
+                  <p>{weather.data[24].weather.description}</p>
+                </div>
+                <div className="p-2">
+                  <img
+                    src={`https://www.weatherbit.io/static/img/icons/${
+                      weather.data[32].weather.icon
+                    }.png`}
+                    alt="icon"
+                  />
+                  <p>{weather.data[32].datetime}</p>
+                  <p>{`${weather.data[32].temp} \u2103`} </p>
+                  <p>{weather.data[32].weather.description}</p>
+                </div>
+                <div className="p-2">
+                  <img
+                    src={`https://www.weatherbit.io/static/img/icons/${
+                      weather.data[39].weather.icon
+                    }.png`}
+                    alt="icon"
+                  />
+                  <p>{weather.data[39].datetime}</p>
+                  <p>{`${weather.data[39].temp} \u2103`} </p>
+                  <p>{weather.data[39].weather.description}</p>
                 </div>
               </div>
             </Fragment>
