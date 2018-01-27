01372223cf1f071e2dc2c6e320667b808010c677
Tomomi <madeinwatashi@gmail.com>
2018-01-27 19:31:46 +1100

    Weather refactor

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -6,15 +6,14 @@ import CustomerTrafficForm from "./CustomerTrafficForm";
 function Home({ signedIn, onCreateCustomerTraffics }) {
   return (
     <div className="row">
-      <div className="col" id="traffic">
+      <div className="col-lg-5" id="traffic">
         <CustomerTrafficForm onChange={onCreateCustomerTraffics} />
         <hr />
       </div>
-      <div className="col" id="">
+      <div className="col-lg-7" id="">
         <CurrencyConverter />
         <hr />
         <Weather />
-        <hr />
       </div>
     </div>
   );
--- web/src/components/Weather.js
+++ web/src/components/Weather.js
@@ -1,146 +1,85 @@
 import React, { Component, Fragment } from "react";
-import { fetchWeather } from "../api/weather";
+import { fetchDailyWeather, fetchNowWeather } from "../api/weather";
 import moment from "moment";
 
 class Weather extends Component {
-  state = {
-    weather: null,
-    error: null
-  };
+   state = {
+      weatherNow: null,
+      weatherDaily: null,
+      error: null
+   };
 
-  componentDidMount() {
-    fetchWeather()
-      .then(weather => {
-        this.setState({ weather });
-      })
-      .catch(error => {
-        this.setState({ error: error });
-      });
-  }
+   componentDidMount() {
+      fetchDailyWeather()
+         .then(weatherDaily => {
+            const weatherFiveDays = weatherDaily.filter((w, index) => {
+               return index > 0 && index < 6;
+            });
+            this.setState({ weatherDaily: weatherFiveDays });
+         })
+         .catch(error => {
+            this.setState({ error: error });
+         });
+      fetchNowWeather()
+         .then(weatherNow => {
+            this.setState({ weatherNow });
+         })
+         .catch(error => {
+            this.setState({ error: error });
+         });
+   }
 
-  render() {
-    const { weather, error } = this.state;
-    return (
-      <div>
-        <div>
-          {!!error && <p>{error.message}</p>}
-          {!!weather ? (
-            <Fragment>
-              <h3 className="text-center"> {weather.city_name} </h3>
-              <div className="row-fuid text-center">
-                <div className="col-md-6 mx-auto">
-                  <p>
-                    {moment(weather.data[0].datetime.slice(0, -3)).format(
-                      "dddd, MMMM do, gggg"
-                    )}
-                  </p>
-                  <img
-                    src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.data[0].weather.icon
-                    }.png`}
-                    alt="icon"
-                  />
-                  <h2>{`${weather.data[0].temp} \u2103`} </h2>
-                  <p>{weather.data[0].weather.description}</p>
-                </div>
-              </div>
+   render() {
+      const { weatherNow, weatherDaily, error } = this.state;
+      return (
+         <div className="text-center">
+            {!!error && <p>{error.message}</p>}
+            {weatherNow && weatherDaily ? (
+               <Fragment>
+                  <h3 className="py-4"> {weatherNow.city_name} </h3>
+                  <div className="row">
+                     <div className="col-6 col-sm-4 offset-sm-2">
+                        <img
+                           src={`https://www.weatherbit.io/static/img/icons/${
+                              weatherNow.weather.icon
+                           }.png`}
+                           alt="weather-icon-now"
+                           className=" pull-right"
+                        />
+                     </div>
+                     <div className="col-6 col-sm-4 text-left pl-0 ml-0 my-auto">
+                        <h2>{`${weatherNow.temp} \u2103`} </h2>
+                        <p>{weatherNow.weather.description}</p>
+                     </div>
+                  </div>
 
-              {/* <div className="d-flex flex-nowrap"> */}
-              <div className="row text-center weekWeather">
-                <div className="col-2" style={{ margin: "auto" }}>
-                  <img
-                    src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.data[8].weather.icon
-                    }.png`}
-                    alt="icon"
-                    style={{ width: "40px" }}
-                  />
-                  <p>
-                    {moment(weather.data[8].datetime.slice(0, -3)).format(
-                      "ddd"
-                    )}
-                  </p>
-                  <strong>{`${weather.data[8].temp} \u2103`}</strong>
-                  <br />
-                  {weather.data[8].weather.description}
-                </div>
-                <div className="col-2" style={{ margin: "auto" }}>
-                  <img
-                    src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.data[16].weather.icon
-                    }.png`}
-                    alt="icon"
-                    style={{ width: "40px" }}
-                  />
-                  <p>
-                    {moment(weather.data[16].datetime.slice(0, -3)).format(
-                      "ddd"
-                    )}
-                  </p>
-                  <strong>{`${weather.data[16].temp} \u2103`}</strong>
-                  <br />
-                  {weather.data[16].weather.description}
-                </div>
-                <div className="col-2" style={{ margin: "auto" }}>
-                  <img
-                    src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.data[24].weather.icon
-                    }.png`}
-                    alt="icon"
-                    style={{ width: "40px" }}
-                  />
-                  <p>
-                    {moment(weather.data[24].datetime.slice(0, -3)).format(
-                      "ddd"
-                    )}
-                  </p>
-                  <strong>{`${weather.data[24].temp} \u2103`}</strong>
-                  <br />
-                  {weather.data[24].weather.description}
-                </div>
-                <div className="col-2" style={{ margin: "auto" }}>
-                  <img
-                    src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.data[32].weather.icon
-                    }.png`}
-                    alt="icon"
-                    style={{ width: "40px" }}
-                  />
-                  <p>
-                    {moment(weather.data[32].datetime.slice(0, -3)).format(
-                      "ddd"
-                    )}
-                  </p>
-                  <strong>{`${weather.data[32].temp} \u2103`}</strong>
-                  <br />
-                  {weather.data[32].weather.description}
-                </div>
-                <div className="col-2" style={{ margin: "auto" }}>
-                  <img
-                    src={`https://www.weatherbit.io/static/img/icons/${
-                      weather.data[39].weather.icon
-                    }.png`}
-                    alt="icon"
-                    style={{ width: "40px" }}
-                  />
-                  <p>
-                    {moment(weather.data[39].datetime.slice(0, -3)).format(
-                      "ddd"
-                    )}
-                  </p>
-                  <strong>{`${weather.data[39].temp} \u2103`}</strong>
-                  <br />
-                  {weather.data[39].weather.description}
-                </div>
-              </div>
-            </Fragment>
-          ) : (
-            <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
-          )}
-        </div>
-      </div>
-    );
-  }
+                  <div className="row">
+                     {weatherDaily.map((weather, index) => {
+                        return (
+                           <ul key={index} id="weather-ul" className="col text-center">
+                              <li>{moment(weather.datetime).format("ddd")}</li>
+                              <li>
+                                 <img
+                                    src={`https://www.weatherbit.io/static/img/icons/${
+                                       weather.weather.icon
+                                    }.png`}
+                                    alt="weather-icon-daily"
+                                    style={{ width: "40px" }}
+                                 />
+                              </li>
+                              <li>{`${weather.max_temp}\u2103`}</li>
+                              <li className="text-muted">{`${weather.min_temp}\u2103`}</li>
+                           </ul>
+                        );
+                     })}
+                  </div>
+               </Fragment>
+            ) : (
+               <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
+            )}
+         </div>
+      );
+   }
 }
 
 export default Weather;
