b3af73bd4b9f703ad9602ad861f77ac9b94bd2ff
Tomomi <madeinwatashi@gmail.com>
2018-02-17 14:14:49 +1100

    Style sign in

--- web/src/components/SignInForm.js
+++ web/src/components/SignInForm.js
@@ -3,53 +3,57 @@ import logo from "../logo.png";
 import { Link } from "react-router-dom";
 
 function SignInForm({ onSignIn }) {
-	return (
-		<div className="row mt-5">
-			<div className="login-screen bg-white box-shadow">
-				<img src={logo} className="logo" alt="logo" /> <small>Management System</small>
-				<h2 className="text-center mt-4 text-shadow">Sign In</h2>
-				<form
-					onSubmit={event => {
-						// Prevent old-school form submission
-						event.preventDefault();
+   return (
+      <div className="row mt-5">
+         <div className="login-screen bg-white box-shadow">
+            <img src={logo} className="logo" alt="logo" /> <small>Management System</small>
+            <h2 className="text-center m-4 text-shadow">Sign In</h2>
+            <form
+               className="col-md-6 offset-md-3"
+               onSubmit={event => {
+                  // Prevent old-school form submission
+                  event.preventDefault();
 
-						const form = event.target;
-						const elements = form.elements; // Allows looking up fields using their 'name' attributes
-						// Get entered values from fields
-						const username = elements.username.value;
-						const password = elements.password.value;
+                  const form = event.target;
+                  const elements = form.elements; // Allows looking up fields using their 'name' attributes
+                  // Get entered values from fields
+                  const username = elements.username.value;
+                  const password = elements.password.value;
 
-						// Pass this information along to the parent component
-						onSignIn({ username, password });
-					}}
-				>
-					<div className="form-group ">
-						<label className="control-label ">Username</label>
-						<div className="input-group">
-							<input type="text" name="username" required />
-						</div>
-					</div>
-
-					<div className="form-group ">
-						<label className="control-label">Password</label>
-						<div className="input-group">
-							<input type="password" name="password" required />
-						</div>
-					</div>
-
-					<div className="form-group  ">
-						<button className="btn btn-primary">Sign In</button>
-					</div>
-					<div className="form-group  ">
-						<Link to="/signup" className="">
-							<i className="fa fa-user-plus" />
-							<span> Sign Up</span>
-						</Link>
+                  // Pass this information along to the parent component
+                  onSignIn({ username, password });
+               }}
+            >
+               <div className="col-auto">
+                  <div className="input-group my-3">
+                     <div className="input-group-prepend">
+                        <div className="input-group-text"><i className="fa fa-user" /></div>
+                     </div>
+                     <input type="text" name="username" className="form-control" required />
                   </div>
-				</form>
-			</div>
-		</div>
-	);
+               </div>
+               <div className="col-auto">
+                  <div className="input-group my-3">
+                     <div className="input-group-prepend">
+                        <div className="input-group-text"><i className="fa fa-unlock-alt" />&nbsp;</div>
+                     </div>
+                     <input type="password" name="password" className="form-control" required />
+                  </div>
+               </div>
+
+               <div className="col-auto my-3">
+                  <button type="submit" className="btn btn-primary mb-2">Sign In</button>
+               </div>
+               <div className="col-auto">
+                  <Link to="/signup" className="">
+                     <i className="fa fa-user-plus" />
+                     <span> Add user</span>
+                  </Link>
+               </div>
+            </form>
+         </div>
+      </div>
+   );
 }
 
 export default SignInForm;
--- web/src/components/SignUpForm.js
+++ web/src/components/SignUpForm.js
@@ -4,52 +4,53 @@ import { Link } from "react-router-dom";
 
 function SignUpForm({ onSignUp }) {
    return (
-      <div className="container-fluid ">
-         <div className="row  mt-5">
-            <div className="login-screen bg-white box-shadow">
-               <img src={logo} className="logo" alt="logo" />{" "}
-               <small>TANTO Management System</small>
-               <h2 className="text-center mt-4 text-shadow">Sign Up</h2>
-               <form
-                  onSubmit={event => {
-                     // Prevent old-school form submission
-                     event.preventDefault();
+      <div className="row mt-5">
+         <div className="login-screen bg-white box-shadow">
+            <img src={logo} className="logo" alt="logo" /> <small>Management System</small>
+            <h2 className="text-center m-4 text-shadow">Add User</h2>
+            <form
+               className="col-md-6 offset-md-3"
+               onSubmit={event => {
+                  // Prevent old-school form submission
+                  event.preventDefault();
 
-                     const form = event.target;
-                     const elements = form.elements; // Allows looking up fields using their 'name' attributes
-                     // Get entered values from fields
-                     //   const email = elements.email.value;
-                     const username = elements.username.value;
-                     const password = elements.password.value;
+                  const form = event.target;
+                  const elements = form.elements; // Allows looking up fields using their 'name' attributes
+                  // Get entered values from fields
+                  const username = elements.username.value;
+                  const password = elements.password.value;
 
-                     // Pass this information along to the parent component
-                     onSignUp({ username, password });
-                  }}
-               >
-                  <fieldset>
-                     <div className="form-group">
-                        <div className="col-6 col-sm-4 col-md-3 col-lg-2">
-                           <label className="mb-2">
-                              {"User name: "}
-                              <input type="text" name="username" />
-                           </label>
-                           <label className="mb-2">
-                              {"Password: "}
-                              <input type="password" name="password" />
-                           </label>
-
-                           <button className="btn btn-info">Sign Up</button>
-                        </div>
+                  // Pass this information along to the parent component
+                  onSignUp({ username, password });
+               }}
+            >
+               <div className="col-auto">
+                  <div className="input-group my-3">
+                     <div className="input-group-prepend">
+                        <div className="input-group-text"><i className="fa fa-user" /></div>
                      </div>
-                  </fieldset>
-
-                  <div>
-                     <Link to="/signin">
-                        <i className="fa fa-arrow-left mt-3 ml-3" /> Back
-              </Link>
+                     <input type="text" name="username" className="form-control" />
                   </div>
-               </form>
-            </div>
+               </div>
+               <div className="col-auto">
+                  <div className="input-group my-3">
+                     <div className="input-group-prepend">
+                        <div className="input-group-text"><i className="fa fa-unlock-alt" />&nbsp;</div>
+                     </div>
+                     <input type="password" name="password" className="form-control" />
+                  </div>
+               </div>
+
+               <div className="col-auto my-3">
+                  <button type="submit" className="btn btn-info mb-2">Add user</button>
+               </div>
+
+               <div>
+                  <Link to="/signin">
+                     <i className="fa fa-arrow-left mt-3 ml-3" /> Back
+                  </Link>
+               </div>
+            </form>
          </div>
       </div>
    );
